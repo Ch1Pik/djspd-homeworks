bb6fecbecaf23a95f25a12b7712b0ab643628209
Sergey Novikov <149697882+Ch1Pik@users.noreply.github.com>
2024-03-07 09:35:06 +0300

    Update serializers.py

--- 3.1/products/main/serializers.py
+++ 3.1/products/main/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 
+from main.models import Product, Review
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     # реализуйте все поля
-    pass
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 
 class ProductListSerializer(serializers.Serializer):
     # реализуйте поля title и price
-    pass
+    title = serializers.CharField()
+    price = serializers.IntegerField()
 
 
 class ProductDetailsSerializer(serializers.ModelSerializer):
     # реализуйте поля title, description, price и reviews (список отзывов к товару)
-    pass
+    comments = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'price', 'comments']

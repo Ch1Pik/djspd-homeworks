b16412cd443757f7c99bdcf870821c5e75af85d0
Sergey Novikov <149697882+Ch1Pik@users.noreply.github.com>
2024-03-18 11:46:21 +0300

    Create serializers.py

---
+++ 3.3/advertisements/serializers.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+
+        # TODO: добавьте требуемую валидацию
+        if self.context["request"].method == "POST" or data.get('status') == 'OPEN':
+            # Количество открытых объявлений текущего пользователя
+            open_count = Advertisement.objects.filter(status='OPEN', creator=self.context["request"].user.id).count()
+            if open_count > 9:
+                raise ValueError('Превышено количество открытых объявлений')
+
+        return data
